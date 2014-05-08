e7d43a6f01108648c25931b086b983721167bc8b
lpilkin3 <lpilkin3@masonlive.gmu.edu>
2014-05-08 09:09:15 -0400

    background effects

--- assets/effects/Rain.js
+++ assets/effects/Rain.js
@@ -1,43 +1,68 @@
-<div class="lluvia"></div><style>
-.gotas {
-  background:-webkit-gradient(linear,0% 0%,0% 100%, from(rgba(13,52,58,1) ), to(rgba(255,255,255,0.6))  );
-  background: -moz-linear-gradient(top, rgba(13,52,58,1) 0%, rgba(255,255,255,.6) 100%);
- width:1px;height:89px;
- position: absolute;top:0px;z-index:99999;
- -webkit-animation: fall .63s linear infinite;
-  -moz-animation: fall .63s linear infinite;
-}
-@-webkit-keyframes fall {
- to {margin-top:900px;}
-}
-@-moz-keyframes fall {
- to {margin-top:900px;}
-}
-</style><script type="text/javascript">
-  //<![CDATA[
-(function() {
+// Rain.js
+// Copyright © Julio Cesar Ody
+(function(window, undefined) {
+  if (typeof Raphael == 'undefined') throw "Rain needs Raphaël.js!";
 
-var nbDrop = 200; 
+  Rain = function(element, speed, angle) {
+    if (!(this instanceof arguments.callee)) { return new arguments.callee(arguments); };
+    var self = this;
 
-function randRange( minNum, maxNum) {
-  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
-}
+    self.init = function(element, config) {
+      var defaults = {speed: 500, angle: 20, intensity: 5, size: 10, color: '#fff'};
+      if (typeof config == 'undefined') {
+        config = defaults;
+      } else {
+        for (var property in defaults) {
+          if (typeof config[property] == 'undefined') config[property] = defaults[property];
+        };
+      }
+      self.config = config;
+      var elt = document.getElementById(element);
+      self.stage = {
+          element:  elt,
+          width:    function() { return elt.getBoundingClientRect().width; },
+          height:   function() { return elt.getBoundingClientRect().height; }
+        };
+      self.canvas = Raphael(self.stage.element);
+      self.offset = (Math.tan(config.angle * Math.PI / 180) * self.stage.height());
+      runEngine();
+      return self;
+    };
 
-function createRain() {
+    function runEngine() {
+      self.enginePid = setInterval(function() { createDrop(self.config); }, 100 / self.config.intensity);
+    }
 
- for( i=1;i<nbDrop;i++) {
- var dropLeft = randRange(0,980);
- var dropTop = randRange(-1000,1400);
+    function randomStartingPoint(angle) {
+      return Math.floor(Math.random() * (self.stage.width() + self.offset));
+    }
 
- $('.lluvia').append('<div class="gotas" id="gotas'+i+'"></div>');
- $('#gotas'+i).css('left',dropLeft);
- $('#gotas'+i).css('top',dropTop);
- }
+    function createPositionMatrix(angle, size) {
+      return [randomStartingPoint(angle), 0, size * 0.1, size];
+    }
 
-}
+    function createDrop(config) {
+      var factor = Math.random(),
+        positionMatrix = createPositionMatrix(config.angle, config.size),
+        drop = self.canvas.ellipse.apply(self.canvas, positionMatrix),
+        layer = config.speed * (1 + factor),
+        cx = positionMatrix[0] - (Math.tan(config.angle * Math.PI / 180) * self.stage.height());
+      drop
+        .attr({stroke: config.color, opacity: 1 - factor, fill: config.color})
+        .rotate(config.angle)
+        .animate({cy: self.stage.height(), cx: cx}, layer, function() { drop.remove(); });
+      return drop;
+    }
 
-createRain();
+    // "class" methods
+    self.setIntensity = function(intensity) {
+      clearInterval(self.enginePid);
+      self.config.intensity = intensity;
+      self.enginePid = setInterval(function() { createDrop(self.config); }, 100 / self.config.intensity);
+    };
 
-})();
-//]]>
-</script>
+    window.onresize = function() { self.canvas.setSize(window.innerWidth, window.innerHeight); };
+
+    return self.init.apply(self, arguments);
+  };
+})(window);
